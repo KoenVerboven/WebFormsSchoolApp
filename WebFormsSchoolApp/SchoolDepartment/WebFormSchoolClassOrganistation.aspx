c4d69c00d402c77fa7d4b77ce6f16c028246d00a
Verboven Koen <koenverboven@gmail.com>
2024-10-23 22:38:49 +0200

    classorganisation basic functions

--- WebFormsSchoolApp/SchoolDepartment/WebFormSchoolClassOrganistation.aspx
+++ WebFormsSchoolApp/SchoolDepartment/WebFormSchoolClassOrganistation.aspx
@@ -1,10 +1,10 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="WebFormSchoolClassOrganistation.aspx.cs" Inherits="WebFormsSchoolApp.SchoolDepartment.WebFormSchoolClassOrganistation" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
     <p>
-        <asp:Button ID="Button1" runat="server" Text="Add Student to Class" OnClick="Button1_Click" />
+       
     </p>
      <p>
-     <asp:Button ID="Button2" runat="server" Text="Remove Student from Class" OnClick="Button2_Click"  />
+    
     </p>
      <div class="row">
         <div class="col-md-1">
@@ -13,11 +13,17 @@
              <asp:Label ID="Label2" runat="server" style="width:200px;background-color:red;color:white" Text="Student" Width="200px"></asp:Label>
              <asp:ListBox ID="ListBox1" style="width:200px" runat="server"></asp:ListBox>
          </div>
+         <div class="col-md-1">
+             <br />
+             <asp:Button ID="Button1" runat="server" Text="-->" OnClick="Button1_Click" />
+             <br /><br />
+             <asp:Button ID="Button2" runat="server" Text="<--" OnClick="Button2_Click"  />
+         </div>
          <div class="col-md-2">
              <asp:Label ID="Label4" runat="server" style="width:200px;background-color:red;color:white" Text="Class" Width="200px"></asp:Label>
              <asp:ListBox ID="ListBox2" style="width:200px" runat="server"></asp:ListBox>
          </div>
-        <div class="col-md7">
+        <div class="col-md6">
         </div>  
     </div>
 
--- WebFormsSchoolApp/SchoolDepartment/WebFormSchoolClassOrganistation.aspx.designer.cs
+++ WebFormsSchoolApp/SchoolDepartment/WebFormSchoolClassOrganistation.aspx.designer.cs
@@ -15,24 +15,6 @@ namespace WebFormsSchoolApp.SchoolDepartment
     {
 
         /// <summary>
-        /// Button1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button1;
-
-        /// <summary>
-        /// Button2 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button Button2;
-
-        /// <summary>
         /// Label2 control.
         /// </summary>
         /// <remarks>
@@ -49,6 +31,24 @@ namespace WebFormsSchoolApp.SchoolDepartment
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ListBox ListBox1;
+
+        /// <summary>
+        /// Button1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button1;
+
+        /// <summary>
+        /// Button2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button Button2;
 
         /// <summary>
         /// Label4 control.
